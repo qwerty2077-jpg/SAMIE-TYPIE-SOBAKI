c35ad06ddd0633e74d6be47d34b567c703ce5672
qwerty2077-jpg <vspilkov573@gmail.com>
2025-02-16 12:27:37 +0300

    Add files via upload

--- lesson_14.py
+++ lesson_14.py
@@ -1,28 +1,28 @@
 import os
 import requests
 import shutil
-folder = "КАРТИНКЭЭЭЭ"
-url = 'https://random.dog/woof.json/'
-payload = {
+FOLDER = "КАРТИНКЭЭЭЭ"
+URL = 'https://random.dog/woof.json/'
+PAYLOAD = {
     "filter": "mp4,webm"
 }
-def download_picture(folder, file_name, url):
-    file_path = os.path.join(folder, file_name)
-    response = requests.get(url,params=payload)
+
+def download_picture(FOLDER, file_name, URL):
+    file_path = os.path.join(FOLDER, file_name)
+    response = requests.get(URL,params=PAYLOAD) 
     with open(file_path, 'wb') as file:
         file.write(response.content)
-
-
 def main():
-    if os.path.isdir(folder):
-        shutil.rmtree(folder)
-    os.mkdir(folder)
+    if os.path.isdir(FOLDER):
+        shutil.rmtree(FOLDER)
+    os.mkdir(FOLDER)
     for number in range(50):
-        response = requests.get(url,params=payload)
+        response = requests.get(URL,params=PAYLOAD)    
         response.raise_for_status()
-        picture_link = response.json()['url']
+        picture_link = response.json()['URL']
         link, picture_extension = os.path.splitext(picture_link)
         file_name = f'dog_{number+1}{picture_extension}'
-        download_picture(folder, file_name, picture_link)
+        download_picture(FOLDER, file_name, picture_link)
+
 if __name__ == '__name__':
     main()
