ec475934b7b7fa29cf2e386d3e5d563abc3dd8f4
qwerty2077-jpg <vspilkov573@gmail.com>
2025-03-02 12:51:28 +0300

    lesson_14.py

--- lesson_14.py
+++ lesson_14.py
@@ -1,28 +1,36 @@
 import os
 import requests
 import shutil
+
+
 FOLDER = "КАРТИНКЭЭЭЭ"
 URL = 'https://random.dog/woof.json/'
 PAYLOAD = {
     "filter": "mp4,webm"
 }
 
+
 def download_picture(FOLDER, file_name, URL):
     file_path = os.path.join(FOLDER, file_name)
-    response = requests.get(URL,params=PAYLOAD) 
+    response = requests.get(URL, params=PAYLOAD) 
     with open(file_path, 'wb') as file:
         file.write(response.content)
+
+
 def main():
     if os.path.isdir(FOLDER):
         shutil.rmtree(FOLDER)
     os.mkdir(FOLDER)
+    print(FOLDER)
     for number in range(50):
-        response = requests.get(URL,params=PAYLOAD)    
+        response = requests.get(URL, params=PAYLOAD)    
         response.raise_for_status()
-        picture_link = response.json()['URL']
+        picture_link = response.json()['url']
         link, picture_extension = os.path.splitext(picture_link)
         file_name = f'dog_{number+1}{picture_extension}'
+        print(file_name)
         download_picture(FOLDER, file_name, picture_link)
 
-if __name__ == '__name__':
+
+if __name__ == '__main__':
     main()
